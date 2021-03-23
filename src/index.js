a6bf1d01da0f13e9b94ad01b9de12ea6b03d3d75
abad89 <abadalamenti89@gmail.com>
2021-03-23 02:11:50 -0400

    begin moving divs to js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const intro = document.createElement('div');
+document.body.appendChild(intro);
+intro.className='intro';
+intro.innerHTML=`<h1>Anthony Badalamenti</h1><h2>Software Development Student</h2>`
