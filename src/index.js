31a92524e6071a4a11a29897713b370f00f8e9ab
abad89 <abadalamenti89@gmail.com>
2021-03-23 03:28:01 -0400

    change font and slightly modify bio text

--- src/index.js
+++ src/index.js
@@ -12,4 +12,4 @@ intro.innerHTML=`<h1>Anthony Badalamenti</h1><h2>Software Development Student</h
 const bio = document.createElement('div');
 document.body.appendChild(bio);
 bio.className='bio';
-bio.innerHTML=`<p>Hi, thanks for checking out my website! I\'m currently in the process of learning HTML, CSS, and Javascript. I am applying what I have learned so far to create this page!`;
+bio.innerHTML=`<p>Hi, thanks for checking out my website! I\'m currently in the process of learning HTML, CSS, and Javascript. I am applying what I have learned so far to create this page.`;
