353aeb4131d3f4d031b3583044ffa12ee788b170
abad89 <abadalamenti89@gmail.com>
2021-03-23 02:18:30 -0400

    add bio div to js, add short bio text

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ const intro = document.createElement('div');
 document.body.appendChild(intro);
 intro.className='intro';
 intro.innerHTML=`<h1>Anthony Badalamenti</h1><h2>Software Development Student</h2>`
+
+const bio = document.createElement('div');
+document.body.appendChild(bio);
+bio.className='bio';
+bio.innerHTML=`<p>Hi, thanks for checking out my website! I\'m currently in the process of learning HTML, CSS, and Javascript. I am applying what I have learned so far to create this page!`;
