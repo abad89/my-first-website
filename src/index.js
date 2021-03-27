3b203321a0e355ce61d073d0786207f1d699931b
abad89 <abadalamenti89@gmail.com>
2021-03-27 07:01:22 -0400

    reword bio

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ intro.innerHTML=`<h1>Anthony Badalamenti</h1><h2>Software Development Student</h
 const bio = document.createElement('div');
 document.body.appendChild(bio);
 bio.className='bio';
-bio.innerHTML=`<p>Hi, thanks for checking out my website! I\'m currently in the process of learning HTML, CSS, and Javascript. I am applying what I have learned so far to create this page.`;
+bio.innerHTML=`<p>Hi, thanks for checking out my website! I\'m currently in the process of learning HTML, CSS, and Javascript. I am applying some of what I have learned so far to create this page.`;
 
 const links = document.createElement('section');
 document.body.appendChild(links);
